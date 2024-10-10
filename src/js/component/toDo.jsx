de5d1229fcf33fbb38ef8cdd2970265a1b63e63b
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-10-10 03:05:25 +0000

    initialized toDoList and finished

---
+++ src/js/component/toDo.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import ToDoForm from "./toDoForm"
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from 'react-icons/ti'
+
+
+const toDo = ({toDos, completeToDo, removeToDo, updateToDo}) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  })
+
+  const submitUpdate = (value) => {
+    updateToDo(edit.id, value)
+    setEdit ({
+        id: null,
+        value: ''
+    })
+  }
+
+  if(edit.id) {
+    return <ToDoForm edit={edit} onSubmit={submitUpdate}/>
+  }
+
+  return toDos.map((toDo, index) => (
+    <div className={toDo.isComplete ? 'toDo-row complete' : 'toDo-row'} key={index}>
+    
+    <div key={toDo.id} onClick={() => completeToDo(toDo.id)}>
+        {toDo.text}
+    </div>
+    
+    <div className="icons">
+        <RiCloseCircleLine 
+        onClick={() => removeToDo(toDo.id)}
+        className='delete-icon'
+        />
+        <TiEdit onClick={() => setEdit({id : toDo.id, value: toDo.text })}
+        className='delete-icon'/>
+    </div>
+    </div>
+  ))
+}
+
+export default toDo
---
+++ src/js/component/toDoForm.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef }from 'react'
+
+const toDoForm = (props) => {
+  const [input, setInput] = useState(props.edit ? props.edit.value : ' ')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
+  
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random() * 1000), 
+        text: input
+    });
+    
+    setInput('')
+  }
+
+ 
+    return (
+       
+            <form className = "toDo-form" onSubmit={handleSubmit}>
+
+                {props.edit ? (
+                <>    
+                <input 
+                className="toDo-input edit" 
+                type="text" 
+                value={input}
+                placeholder="Update" 
+                onChange={handleChange}
+                ref={inputRef}
+                />
+                <button type="button" className="btn edit btn-outline-info">Update</button>
+                </>
+                )
+                 :
+                (
+                <>
+                <input 
+                className="toDo-input" 
+                type="text" 
+                value={input}
+                placeholder="Add a todo" 
+                aria-label="default input example"
+                onChange={handleChange}
+                ref={inputRef}
+                />
+                <button type="button" className="btn btn-outline-info">Add ToDo </button>
+                </>
+                )
+            }
+
+                
+            </form>
+        
+  )
+}
+
+export default toDoForm
